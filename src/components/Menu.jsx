27b6dab67219b09e26698632a607f5b1ba5d1d4e
Raximjon <raximjonr105@gmail.com>
2025-02-28 20:07:09 +0500

    Yangi o'zgarishlar qo'shildi

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -12,12 +12,12 @@ const Menu = () => {
   }, {});
 
   return (
-    <div className="p-4 flex flex-col items-center">
+    <div className="max-w-[30rem] flex flex-col items-center mx-auto">
       <h1 className="text-2xl font-bold">Menu</h1>
       {Object.keys(groupedItems).map((type) => (
-        <div key={type} className="w-full mt-6">
-          <h2 className="text-xl font-semibold mb-4">{type}</h2>
-          <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 place-items-center">
+        <div key={type} className="w-[18rem] mt-6">
+          <h2 className="text-xl font-semibold mb-2">{type}</h2>
+          <div className="grid grid-cols-2 gap-2 place-items-center">
             {groupedItems[type].map((item) => (
               <FoodCard key={item.id} {...item} />
             ))}
